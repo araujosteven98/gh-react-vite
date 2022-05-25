cd636f116231b144dc073f3c2b92f7098115c57b
Steven Araujo <steven.araujo@nodel.co>
2022-05-25 11:52:56 -0500

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://gndx.github.io/gh-react-vite/',
+  base: 'https://araujosteven98.github.io/gh-react-vite/',
   plugins: [react()]
 })
